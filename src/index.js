26ec1436da12d59dd4448008edcde41df48594a3
Robin Kaga <robin.kaga@no.experis.com>
2025-02-05 08:28:32 +0100

    core done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
+var cards = document.getElementById("cardList");
 
-console.log(data);
+function main(){
+    for (let i = 0; i < data.length; i++){
+        var card = document.createElement("li");
+        card.classList.add("card");
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+        var pokName = document.createElement("h2");
+        pokName.classList.add("card--title");
+        pokName.textContent = data[i].name;
+        card.appendChild(pokName);
+
+        var image = document.createElement("img");
+        image.classList.add("card--img");
+        image.width = 256;
+        image.src = data[i].sprites.front_default;
+        card.appendChild(image);
+
+        var stats = document.createElement("ul");
+        stats.classList.add("card--text");
+        for (let j = 0; j < data[i].stats.length; j++){
+            var stat = document.createElement("li");
+            stat.classList.add("stat--text");
+            stat.textContent = data[i].stats[j].stat.name.toUpperCase() + ": " + data[i].stats[j].base_stat;
+            stats.appendChild(stat);
+        }
+        card.appendChild(stats);
+
+        cards.appendChild(card);
+    }    
+
+}
+main()
